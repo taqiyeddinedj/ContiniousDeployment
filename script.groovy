941d55185a2bd9a991a8b2bc4b8d7bba000c6bda
taqiyeddinedj <djouani.taqiyeddine@gmail.com>
2024-01-11 12:03:52 +0100

    changing the kuberneteDeploy method in the groovy script file

--- script.groovy
+++ script.groovy
@@ -9,7 +9,7 @@ def buildDockerImage() {
 
 def deploytok8s() {
     echo "Deploying now the apllication on the kubernetes cluster"
-    kubernetesDeploy configs: 'webapp-deployment.yaml', kubeConfig: [path: ''], kubeconfigId: 'kubernetes', secretName: '', ssh: [sshCredentialsId: '*', sshServer: ''], textCredentials: [certificateAuthorityData: '', clientCertificateData: '', clientKeyData: '', serverUrl: 'https://']
+    kubernetesDeploy configs: 'webapp-deployment.yaml',kubeconfigId: 'kubernetes'
 }
 
 return this
